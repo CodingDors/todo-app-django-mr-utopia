61cf4de991aa38e2bd5856903375ea70b84cc72d
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2023-11-20 21:19:34 -0300

    Update tests.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -17,3 +17,19 @@ class ToDoAppTestCase(TestCase):
         response = self.client.post(self.index_url, {'new_task': 'Do laundry'})
         self.assertEqual(response.status_code, 200)
         self.assertIn('Do laundry', response.context['tasks'])
+
+    def test_task_removal(self):
+        # Step 1: Add a new task
+        task_name = 'Test Task'
+        self.client.post(self.index_url, {'new_task': task_name})
+    
+        # Step 2: Verify the task is added
+        response = self.client.get(self.index_url)
+        self.assertIn(task_name, response.content.decode())
+    
+        # Step 3: Remove the task
+        self.client.post(reverse('remove'), {'task': task_name})
+    
+        # Step 4: Verify the task is removed
+        response = self.client.get(self.index_url)
+        self.assertNotIn(task_name, response.content.decode())
