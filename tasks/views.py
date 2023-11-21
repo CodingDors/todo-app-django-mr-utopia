07c451085edda95902f7b2cd82432bb434b926a8
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2023-11-20 21:16:12 -0300

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,29 @@ tasks = []
 
 # Create your views here.
 def index(request):
-    # TODO: GET method - improve this function to send the tasks list to the HTML file
+    """ 
+    Handle GET Requests in index Function:
+        Add a conditional check for the request method.
+        If the method is "GET", use `render` function to render "index.html" and pass the tasks list as a variable.
+    """
+    # TODO
+    
+    """
+    Handle POST Requests in index Function for Adding Tasks:
+        Still in the index function, add an else block to handle POST requests.
+        Retrieve the task from the form data using request.POST["task"].
+        Append the retrieved task to the tasks list.
+        Finally, use `render` function to render "index.html" and pass the tasks list as a variable.
+    """
+    # TODO
     return render(request, "tasks/index.html")
 
-    # TODO: POST method - handle the submission of new tasks and redirect to index function
+def remove(request):
+    """
+    Handle POST Requests in remove Function:
+        In the remove function, add code to handle POST requests.
+        Retrieve the task to be removed from the form data using request.POST["task"].
+        Remove the specified task from the tasks list using tasks.remove(task).
+        Finally, use `render` function to render "index.html" and pass the tasks list as a variable.
+    """
+    # TODO
